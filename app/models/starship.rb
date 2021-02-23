d384086d782ebacb60b8c35cb556a6d22937dacf
Grzegorz Wiktorzak <wiktorzak.greg@gmail.com>
2021-02-23 12:16:40 +0100

    validates for name cost crew passengers cargo_capacity

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,4 +1,10 @@
 class Starship < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :cost, presence: true
+  validates :crew, presence: true
+  validates :passengers, presence: true
+  validates :cargo_capacity, presence: true
 end
