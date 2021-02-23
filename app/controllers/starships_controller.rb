8a730c1013e713dcb37f131a263c0e147613b4a8
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-23 12:03:20 +0000

    Add #new method to starships. Create #new form in _form.html.erb. Create #new page in new.html.erb. Add Devise authentications in application controller and starships controller. Add new and create routes.

--- app/controllers/starships_controller.rb
+++ app/controllers/starships_controller.rb
@@ -1,8 +1,26 @@
 class StarshipsController < ApplicationController
+skip_before_action :authenticate_user!, only: [:show, :index]
+
   def index
     @starships = Starship.all
   end
+
   def show
     @starship = Starship.find(params[:id])
+  end
+
+  def new
+    @starship = Starship.new
+  end
+
+  def create
+    @starship = Starship.new(starship_params)
+
+  end
+
+  private
+
+  def starship_params
+    params.require(:starship).permit(:name, :model, :manufacturer, :cost, :length, :max_speed, :crew, :passengers, :cargo_capacity, :starship_class, :location, :photo)
   end
 end
