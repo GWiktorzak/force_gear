3876431d54518b697a6ba59c87ce362d102b1605
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-23 16:10:58 +0000

    Add #new and #create methods to bookings. Create view and form for bookings #new. Add new and create routes. Add new booking button in starship show.html.erb.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  def new
+    find_starship
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params.merge(user_id: current_user.id))
+    find_starship
+    @booking.starship = @starship
+    if @booking.save!
+      redirect_to starship_path(@starship)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_starship
+    @starship = Starship.find(params[:starship_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:booking_start_date, :booking_end_date)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :starship
+
+  validates :booking_start_date, :booking_end_date, presence: true
 end
