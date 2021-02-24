cbca2402097f7a31086a7346e3bce8f54d727280
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-24 12:01:40 +0000

    Add bookings #destroy. Add delete button to bookings index page. Fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :starships, only: %i[index show new create edit update destroy] do
     resources :bookings, only: %i[new create]
   end
-  resources :bookings, only: %i[index, destroy]
+  resources :bookings, only: %i[index destroy]
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
