b85fa506bdea7aa1ca467ebf432402ee5a9b4de9
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-25 12:05:07 +0000

    Rewrite seeds to reflect changes to starship model

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,23 @@ number = 1
   starships_resource = JSON.parse(starships_seed)
   data = starships_resource['results']
   user_id = 1
-  location = "London"
+  location = %s[London Paris Berlin Vienna Rome Madrid Lisbon Warsaw]
+  lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+  rating = (0..5).to_a
 
   data.each do |starship|
     starship = Starship.create!(
       name: starship['name'],
-      model: starship['model'],
-      manufacturer: starship['manufacturer'],
       cost: starship['cost_in_credits'],
-      length: starship['length'],
-      max_speed: starship['max_atmosphering_speed'],
-      crew: starship['crew'],
-      passengers: starship['passengers'],
-      cargo_capacity: starship['cargo_capacity'],
-      starship_class: starship['starship_class'],
+      description: lorem,
+      rating: rating,
       user_id: User.last.id,
-      location: location
+      location: location.sample
     )
     user_id += 1
     p starship.id
   end
   number += 1
 end
+
+puts "Finished"
