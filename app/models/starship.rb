af9adf3a0fe7167ff57152b561284f840116599a
Peter Wallace <peterwallace@hotmail.co.uk>
2021-02-25 11:52:59 +0000

    Remove validation in starship limiting locations

--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,12 +1,11 @@
 class Starship < ApplicationRecord
-  LOCATION = %w[London Birmingham Manchester Bristol Liverpool Nottingham Cardiff Newcastle]
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
   validates :name, presence: true
   validates :cost, presence: true
   validates :description, presence: true
-  validates :location, presence: true, inclusion: { in: LOCATION }
+  validates :location, presence: true
 
   has_one_attached :photo
 end
